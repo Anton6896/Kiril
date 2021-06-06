57e09eab76ecac3ad51b80e827b0e2cfe59b3785
AntLinux <anton-r@live.com>
2021-06-06 20:45:59 +0300

    repare timing

--- statistic/utils.py
+++ statistic/utils.py
@@ -1,22 +1,27 @@
 from decimal import *
 from .models import Statistic
 from django.core import serializers
-import datetime
 from django.utils.dateparse import parse_date
+import datetime
 
 
 def _order_util(start, end, order):
     """
     return qs in order if user asked
+    !!! because of my not understanding or bug (i opened at repo)
+    i to (+ datetime.timedelta(days=1)) to end date
+    Django didnt return date_lte !!!!
     """
     start_date = parse_date(start)
-    end_date = parse_date(end)
-
+    end_date = parse_date(end) + datetime.timedelta(days=1)
 
     if order:
         qs_actual = Statistic.objects.filter(
+            # date__gte=start_date, date__lte=end_date
             date__gte=start_date, date__lte=end_date
         ).order_by(str(order))
+
+        print(qs_actual.query)
     else:
         qs_actual = Statistic.objects.filter(
             date__gte=start_date, date__lte=end_date
