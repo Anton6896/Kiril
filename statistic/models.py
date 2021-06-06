e5e0aa40cecd3ed19360d8eecbc367f1fbed35a0
AntLinux <anton-r@live.com>
2021-06-06 19:47:31 +0300

    repare timing

--- statistic/models.py
+++ statistic/models.py
@@ -1,5 +1,7 @@
+
 from django.db import models
 from django.db.models.signals import post_save
+from django.utils import timezone
 
 
 class Statistic(models.Model):
@@ -10,7 +12,7 @@ class Statistic(models.Model):
     views = models.IntegerField(blank=True, null=True, default=1)
     clicks = models.IntegerField(blank=True, null=True, default=1)
     cost = models.DecimalField(max_digits=6, decimal_places=2, default=1, blank=True, null=True)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(default=timezone.now())
 
     # db name apiaries
     class Meta:
